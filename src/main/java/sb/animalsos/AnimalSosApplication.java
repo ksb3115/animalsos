a056caa9c39c8b454799e3456fe21ed2360ddc90
Setbyeol kwon <itcrowd91@gmail.com>
2023-08-25 18:10:55 +0900

    animal sos 프로젝트 생성

---
+++ src/main/java/sb/animalsos/AnimalSosApplication.java
@@ -0,0 +1,14 @@
+package sb.animalsos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalSosApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(AnimalSosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sb/animalsos/AnimalSosApplicationTests.java
@@ -0,0 +1,13 @@
+package sb.animalsos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalSosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

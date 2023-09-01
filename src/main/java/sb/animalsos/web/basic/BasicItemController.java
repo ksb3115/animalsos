03a7455d188b7ca2bd65b0aa0e14757165f572d1
Setbyeol kwon <itcrowd91@gmail.com>
2023-09-01 18:57:21 +0900

    게시글등록(새로고침시 중복등록 오류 수정)

--- src/main/java/sb/animalsos/web/basic/BasicItemController.java
+++ src/main/java/sb/animalsos/web/basic/BasicItemController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import sb.animalsos.domain.item.Item;
 import sb.animalsos.domain.item.ItemRepository;
 
@@ -47,19 +48,19 @@ public class BasicItemController {
 
         itemRepository.save(item);
 
-        model.addAttribute("item",item);
-
+        //model.addAttribute("item",item); //ModelAttribute는 객체를 만들어주고 model에 넣어주는 역할까지 2가지 다 수행!자동 추가,생략가능
 
         return "basic/item";
     }
     @PostMapping("/add")
-    public String addItemV2(@ModelAttribute Item item ,Model model){
+    public String addItemV2(@ModelAttribute Item item , RedirectAttributes redirectAttributes){
 
-        itemRepository.save(item);
-
+        Item savedItem = itemRepository.save(item);
         //model.addAttribute("item",item); //ModelAttribute는 객체를 만들어주고 model에 넣어주는 역할까지 2가지 다 수행!자동 추가,생략가능
-
-        return "basic/item";
+        //return "basic/item";//새로고침시 같은 데이터 쌓이는 오류
+        redirectAttributes.addAttribute("itemId",savedItem.getId());
+        redirectAttributes.addAttribute("status",true);
+        return "redirect:/basic/items/{itemId}";
     }
     @GetMapping("/{itemId}/edit")
     public String editForm(@PathVariable Long itemId,Model model){

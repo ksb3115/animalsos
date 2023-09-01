2f1f061e1629c08906292332d10d97b734bf5851
Setbyeol kwon <itcrowd91@gmail.com>
2023-09-02 02:13:13 +0900

    스프링 메시지 관리 기능 추가

--- src/main/java/sb/animalsos/web/form/FormItemController.java
+++ src/main/java/sb/animalsos/web/form/FormItemController.java
@@ -1,4 +1,4 @@
-package sb.animalsos.web.basic;
+package sb.animalsos.web.form;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -12,9 +12,9 @@ import javax.annotation.PostConstruct;
 import java.util.List;
 
 @Controller
-@RequestMapping("/basic/items")
+@RequestMapping("/form/items")
 @RequiredArgsConstructor
-public class BasicItemController {
+public class FormItemController {
 
     private final ItemRepository itemRepository;
 
@@ -22,19 +22,20 @@ public class BasicItemController {
     public String items(Model model){
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items", items);
-        return "basic/items";
+        return "form/items";
     }
 
     @GetMapping("/{itemId}")
     public String item(@PathVariable long itemId ,Model model){
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item",item);
-        return "basic/item";
+        return "form/item";
     }
 
     @GetMapping("/add")
-    public String addForm(){
-        return "basic/addForm";
+    public String addForm(Model model){
+        model.addAttribute("item",new Item());
+        return "form/addForm";
     }
     //@PostMapping("/add")
     public String addItemV1(@RequestParam String itemName,
@@ -50,17 +51,17 @@ public class BasicItemController {
 
         //model.addAttribute("item",item); //ModelAttribute는 객체를 만들어주고 model에 넣어주는 역할까지 2가지 다 수행!자동 추가,생략가능
 
-        return "basic/item";
+        return "form/item";
     }
     @PostMapping("/add")
     public String addItemV2(@ModelAttribute Item item , RedirectAttributes redirectAttributes){
 
         Item savedItem = itemRepository.save(item);
         //model.addAttribute("item",item); //ModelAttribute는 객체를 만들어주고 model에 넣어주는 역할까지 2가지 다 수행!자동 추가,생략가능
-        //return "basic/item";//새로고침시 같은 데이터 쌓이는 오류
+        //return "form/item";//새로고침시 같은 데이터 쌓이는 오류
         redirectAttributes.addAttribute("itemId",savedItem.getId());
         redirectAttributes.addAttribute("status",true);
-        return "redirect:/basic/items/{itemId}";
+        return "redirect:/form/items/{itemId}";
     }
     @GetMapping("/{itemId}/edit")
     public String editForm(@PathVariable Long itemId,Model model){
@@ -68,13 +69,13 @@ public class BasicItemController {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
 
-        return "basic/editForm";
+        return "form/editForm";
     }
 
     @PostMapping("/{itemId}/edit")
     public String edit(@PathVariable Long itemId,@ModelAttribute Item item){
         itemRepository.update(itemId,item);
-        return "redirect:/basic/items/{itemId}";
+        return "redirect:/form/items/{itemId}";
     }
 
     @PostConstruct

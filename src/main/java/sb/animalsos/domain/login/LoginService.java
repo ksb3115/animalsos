3525ba950ae0fda402edcb01a210f2e0bd96c8ef
Setbyeol kwon <itcrowd91@gmail.com>
2023-09-08 20:20:27 +0900

    쿠키를 이용한 로그인,로그아웃 처리

---
+++ src/main/java/sb/animalsos/domain/login/LoginService.java
@@ -0,0 +1,23 @@
+package sb.animalsos.domain.login;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import sb.animalsos.domain.member.Member;
+import sb.animalsos.domain.member.MemberRepository;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+
+    private final MemberRepository memberRepository;
+
+    /**
+     * @return null이면 로그인 실패
+     */
+    public Member login(String loginId, String password) {
+        return memberRepository.findByLoginId(loginId)
+                .filter(m -> m.getPassword().equals(password))
+                .orElse(null);
+    }
+}
---
+++ src/main/java/sb/animalsos/web/login/LoginForm.java
@@ -0,0 +1,16 @@
+package sb.animalsos.web.login;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+
+@Data
+public class LoginForm {
+
+    @NotEmpty
+    private String loginId;
+
+    @NotEmpty
+    private String password;
+
+}

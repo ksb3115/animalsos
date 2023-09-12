4e47b27ed641057e514e3ce9315bb122e5040fe1
Setbyeol kwon <itcrowd91@gmail.com>
2023-09-12 23:13:45 +0900

    로그인처리하기-HttpSession 기능추가

--- src/main/java/sb/animalsos/web/login/LoginController.java
+++ src/main/java/sb/animalsos/web/login/LoginController.java
@@ -10,9 +10,13 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import sb.animalsos.domain.login.LoginService;
 import sb.animalsos.domain.member.Member;
+import sb.animalsos.web.SessionConst;
+import sb.animalsos.web.session.SessionManager;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 @Slf4j
@@ -20,6 +24,7 @@ import javax.validation.Valid;
 @RequiredArgsConstructor
 public class LoginController {
 
+    private final SessionManager sessionManager;
     private final LoginService loginService;
 
     @GetMapping("/login")
@@ -29,7 +34,7 @@ public class LoginController {
 
     @PostMapping("/login")
     public String login(@Valid @ModelAttribute LoginForm form, BindingResult
-            bindingResult, HttpServletResponse response) {
+            bindingResult, HttpServletRequest request) {
         if (bindingResult.hasErrors()) {
             return "login/loginForm";
         }
@@ -41,16 +46,20 @@ public class LoginController {
             return "login/loginForm";
         }
         //로그인 성공 처리
-        //쿠키에 시간 정보를 주지 않으면 세션 쿠키(브라우저 종료시 모두 종료)
-        Cookie idCookie = new Cookie("memberId",
-                String.valueOf(loginMember.getId()));
-        response.addCookie(idCookie);
+        //세션이 있으면 있는 세션 반환, 없으면 신규 세션 생성
+        HttpSession session = request.getSession();
+        //세션에 로그인 회원 정보 보관
+        session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
         return "redirect:/";
     }
 
+
     @PostMapping("/logout")
-    public String logout(HttpServletResponse response) {
-        expireCookie(response, "memberId");
+    public String logout(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
         return "redirect:/";
     }
     private void expireCookie(HttpServletResponse response, String cookieName) {
